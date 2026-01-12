005606da19e6bc9b6e85f9b89285d06ea8ec9a54
Micael Cardoso Reis <cardosomicael245@gmail.com>
2026-01-11 21:47:02 -0300

    refactor(presentation): move componentes compartilhados para src/app/components e inicia consolidação de admin page

---
+++ src/app/components/index.ts
@@ -0,0 +1,8 @@
+// src/app/pages/components/index.ts
+// Exportações centralizadas dos componentes de página
+
+export { SubjectCard } from './SubjectCard';
+export { UnitCard } from './UnitCard';
+export { StatsSection } from './StatsSection';
+export { SubjectsList } from './SubjectsList';
+export { UnitsList } from './UnitsList';
